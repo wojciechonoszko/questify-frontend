56f3fee76d59fee54a507f452e0f7708fb0c31f5
Wojciech <wojciechonoszko@gmail.com>
2022-09-16 19:14:29 +0200

    card

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import AddCard from 'components/modal/modal';
+import MediaCard from 'components/cardsToday/cardsToday'
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App>
       <AddCard></AddCard>
+      <MediaCard></MediaCard>
     </App>
   </React.StrictMode>
 );
