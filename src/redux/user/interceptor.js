250a730eb346a929cec470d4e8c6125dcfc009a4
Wojciech <wojciechonoszko@gmail.com>
2022-09-17 13:00:33 +0200

    Create interceptor.js

---
+++ src/redux/user/interceptor.js
@@ -0,0 +1,50 @@
+import axios from "axios";
+
+const BASE_URL = process.env.REACT_APP_BASE_URL;
+
+const api = axios.create({
+    withCredentials: true,
+    baseURL: `${BASE_URL}/users`,
+});
+
+api.interceptors.request.use((config) => {
+    const accessToken = localStorage.getItem('accessToken');
+
+    config.headers.Authorization = `Bearer ${accessToken}`;
+    return config;
+});
+
+api.interceptors.response.use((config) => {
+    return config;
+},
+
+    async (error) => {
+        const originalRequest = error.config;
+        if (
+            error.response.status === 401 &&
+            error.config &&
+            !error.config.secondTime
+        ) {
+            originalRequest.secondTime = true;
+            try {
+                const response = await axios.get(
+                    `${BASE_URL}/users`,
+                    {
+                        withCredentials: true,
+                    }
+                );
+                localStorage.setItem('accessToken', response.data.accessToken);
+                return api.request(originalRequest);
+            } catch(error){
+                
+            }
+            throw error;
+            }
+            
+        }
+);
+
+export default api;
+
+
+    
