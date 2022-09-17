30a8a76c3a081ca5eff5145b23b2b2b18420c62a
Wojciech <wojciechonoszko@gmail.com>
2022-09-17 21:31:24 +0200

    update card and add time picker

---
+++ src/redux/todo/todoSelector.js
@@ -0,0 +1,4 @@
+export const todoActive = state => state?.todo?.activeTodo;
+export const todoDone = state => state?.todo?.completedTodo;
+export const newTodoCard = state => state?.todo?.newTodoCard;
+export const error = state => state?.todo?.error;
