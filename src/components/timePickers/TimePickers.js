43393951db1bbffb3d082454dea598c88ce50695
Wojciech <wojciechonoszko@gmail.com>
2022-09-19 13:37:37 +0200

    change on task

--- src/components/timePickers/TimePickers.js
+++ src/components/timePickers/TimePickers.js
@@ -1,8 +1,37 @@
 import TextField from '@mui/material/TextField';
 import AdapterDateFns from '@mui/lab/AdapterDateFns';
-import {LocalizationProvider} from '@mui/lab';
+import { LocalizationProvider } from '@mui/lab';
 import { DateTimePicker } from '@mui/lab';
 import { useState } from 'react';
 
 import styles from './TimePickers.module.css';
 
+export default function DateAndTimePicker({ time }) {
+  const [value, setValue] = useState('week');
+
+  const onChange = data => {
+    const days = [
+      'Sunday',
+      'Monday',
+      'Tuesday',
+      'Wednesday',
+      'Thursday',
+      'Friday',
+      'Saturday',
+      ];
+      const monthNames = [
+        'January',
+        'February',
+        'March',
+        'April',
+        'May',
+        'June',
+        'July',
+        'August',
+        'September',
+        'October',
+        'November',
+        'December',
+      ];
+  };
+}
