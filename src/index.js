dfd79ebd8d7d81d30dee998f5ee400fefbf056f8
Wojciech <wojciechonoszko@gmail.com>
2022-09-21 19:09:09 +0200

    card card

--- src/index.js
+++ src/index.js
@@ -14,7 +14,11 @@ import MediaCard from 'components/cardsToday/cardsToday'
 >>>>>>> 56f3fee (card)
 =======
 import DateAndTimePicker from 'components/timePickers/TimePickers'
+<<<<<<< HEAD
 >>>>>>> 778541e (time picker)
+=======
+import Card from 'components/card/Card'
+>>>>>>> fce165d (card card)
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -31,7 +35,11 @@ ReactDOM.createRoot(document.getElementById('root')).render(
 >>>>>>> 56f3fee (card)
 =======
       <DateAndTimePicker></DateAndTimePicker>
+<<<<<<< HEAD
 >>>>>>> 778541e (time picker)
+=======
+      <Card></Card>
+>>>>>>> fce165d (card card)
     </App>
   </React.StrictMode>
 );
