3692e1d943b19d9224d90b9c1486f65ea9ad7ca2
Wojciech <wojciechonoszko@gmail.com>
2022-09-26 22:38:28 +0200

    card

--- src/index.js
+++ src/index.js
@@ -10,7 +10,8 @@ import PageHeader from 'components/header/header';
 import PageMain from 'components/main/main';
 import MediaCard from 'components/cardsToday/cardsToday'
 import DateAndTimePicker from 'components/timePickers/TimePickers'
-import Card from 'components/card/Card'
+import CardsToday from 'components/cardsToday/cardsToday'
+//import Card from 'components/card/Card'
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -23,7 +24,8 @@ ReactDOM.createRoot(document.getElementById('root')).render(
           <AddCard></AddCard>
           <MediaCard></MediaCard>
           <DateAndTimePicker></DateAndTimePicker>
-          <Card></Card>
+          <CardsToday/>
+          {/* <Card></Card> */}
         </App>
       </PersistGate>
     </Provider>
