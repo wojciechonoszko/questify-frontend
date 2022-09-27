69a908633fc8098f00202afdd54d5b36826119c8
Wojciech <wojciechonoszko@gmail.com>
2022-09-27 21:12:42 +0200

    Update dateAdapted.js

---
+++ src/components/TimePickers/dateAdapted.js
@@ -0,0 +1,51 @@
+const dateDisplay = data => {
+  const days = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ];
+  const monthNames = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
+
+  let dayName = days[data.getDay()];
+  const hours = data.getHours();
+  const minutes = (data.getMinutes() < 10 ? '0' : '') + data.getMinutes();
+  const times = `${hours} : ${minutes}`;
+  const dateNow = new Date().getDate();
+  if (
+    data.getMonth() === new Date().getMonth() &&
+    data.getDate() === new Date().getDate()
+  ) {
+    dayName = 'Today';
+  } else if (
+    data.getMonth() === new Date().getMonth &&
+    new Date().getDate() + 1 === Number(dateNow)
+  ) {
+    dayName = 'Tomorrow';
+  } else if (Number(data.getDate()) >= Number(dateNow) + 7) {
+    const day = data.getDate();
+    const month = monthNames[data.getMonth()];
+    dayName = `${day}. ${month}`;
+  }
+  //   setValue({ dayName });
+  //   time({ time: times, dayName, data });
+  return { time: times, dayName };
+};
+
+export default dateDisplay;
